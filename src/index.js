f94c19e3956b84b566341a43a6d6e44e54fe026e
JosephGlz <joseph220199@gmail.com>
2022-12-07 22:53:14 -0600

    fix logs

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 //estatic the file logs.txt
-app.use(express.static(path.join(__dirname, '../logs.txt')));
+app.use(express.static(path.join(__dirname, './../logs.txt')));
 
 app.use(express.json());
 app.use('/whatsapp', apiRoute);
